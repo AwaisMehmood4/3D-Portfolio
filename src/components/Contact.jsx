b4aa4558ffaadd700f625be1bab82d8bc2dccb32
Awais Mehmood <awaismahmood4@gmail.com>
2024-03-19 11:16:06 +0500

    import emailjs package

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef } from "react"
 import { motion } from "framer-motion"
-// import { emailjs } from '@emailjs/browser';
+import { emailjs } from '@emailjs/browser';
 import { styles } from "../Style";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
